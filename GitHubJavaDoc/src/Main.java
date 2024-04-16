ae83f3c3824139b4eaf99102a26aecdcabc34ea3
rafaelgaln <rafael.galn@educa.madrid.org>
2024-04-16 20:32:15 +0200

    Es navidad vale?

--- GitHubJavaDoc/src/Main.java
+++ GitHubJavaDoc/src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("He creado una clase Main!");
         System.out.println("He conseguido hacer mi primer commit!");
         System.out.println("Intentando hacer mi segundo commit :/");
+        System.out.println("Es navidad xd");
     }
 }
