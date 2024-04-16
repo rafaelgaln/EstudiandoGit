99eeb977ab63fbfd37a6cc15109becbf2f04ceae
rafaelgaln <rafael.galn@educa.madrid.org>
2024-04-16 20:19:19 +0200

    Mi segundo commit chavalada

---
+++ GitHubJavaDoc/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("He creado una clase Main!");
+        System.out.println("He conseguido hacer mi primer commit!");
+        System.out.println("Intentando hacer mi segundo commit :/");
+    }
+}
